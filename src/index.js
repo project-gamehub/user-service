a21d37bc13e6b2e75ccd302fbf7a72500e97d6ed
awezmirza <awezbaigmirza@gmail.com>
2024-04-23 17:28:50 +0530

    Added body parser

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ import router from "./routes/index.js";
 
 const app = express();
 
+// TODO Configure this later
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 // TODO Add Limit on this
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
