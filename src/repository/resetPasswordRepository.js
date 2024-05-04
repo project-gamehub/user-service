a2746cc12a61f2290adbcc1c5b2ebf5b27c37d43
awezmirza <awezbaigmirza@gmail.com>
2024-05-04 16:08:23 +0530

    Wrote the functions of reset pass repository

--- src/repository/resetPasswordRepository.js
+++ src/repository/resetPasswordRepository.js
@@ -1,5 +1,24 @@
 import resetPassword from "../models/resetPasswordModel.js";
 
-class resetPasswordRepository {}
+class resetPasswordRepository {
+    async create(data) {
+        const otpData = await resetPassword.create(data);
+        return otpData;
+    }
+
+    async update(specifierData, dataToUpdate) {
+        const newOtpData = await resetPassword.findOneAndUpdate(
+            specifierData,
+            dataToUpdate,
+            { new: true }
+        );
+        return newOtpData;
+    }
+
+    async getOtpData(specifierData, getFields = "") {
+        const otpData = await resetPassword.findOne(specifierData, getFields);
+        return otpData;
+    }
+}
 
 export default resetPasswordRepository;
